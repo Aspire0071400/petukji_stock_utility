c2ae2499df88b328f0a49f889a982613307490df
Aspire0071400 <prabhujivats@gmail.com>
2024-10-09 16:26:40 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/petukji_stock_utility/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.petukji_stock_utility
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/trade_lab_setup.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TradeLabSetup extends StatelessWidget {
+  const TradeLabSetup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
